3ea0b22b61c627cd0137aba8f6450fcc675396eb
Raghav Joshi <raghavjoshi079@gmail.com>
2020-05-18 15:27:04 +0530

    Colors Colors everywhere

--- lib/regional_data.dart
+++ lib/regional_data.dart
@@ -9,6 +9,52 @@ class RegionalData extends StatefulWidget {
 
 stateCardGenerator() {
   List<Widget> stateCardList = [];
+  List<Color> colors = [
+    Color(0xFFea728c),
+    Color(0xFFffa41b),
+    Color(0xFFff1e56),
+    Color(0xFF00909e),
+    Color(0xFFfe346e),
+    Color(0xFF116979),
+    Color(0xFFde7119),
+    Color(0xFFd7385e),
+    Color(0xFFf0a500),
+    Color(0xFFd9455f),
+    Color(0xFF6f0000),
+    Color(0xFFe43f5a),
+    Color(0xFFf4a548),
+    Color(0xFFfa744f),
+    Color(0xFFc70039),
+    Color(0xFFff5722),
+    Color(0xFF4d4c7d),
+    Color(0xFFff1e56),
+    Color(0xFF000839),
+    Color(0xFFd63447),
+    Color(0xFFc70039),
+    Color(0xFF1eb2a6),
+    Color(0xFFea728c),
+    Color(0xFF111d5e),
+    Color(0xFFd45079),
+    Color(0xFFc70d3a),
+    Color(0xFF6d3580),
+    Color(0xFF018383),
+    Color(0xFFe61c5d),
+    Color(0xFF9a1f40),
+    Color(0xFFf67280),
+    Color(0xFFf0134d),
+    Color(0xFFb5525c),
+    Color(0xFFf57b51),
+    Color(0xFF142850),
+    Color(0xFFfe9801),
+    Color(0xFFff9776),
+    Color(0xFF018383),
+    Color(0xFF21243d),
+    Color(0xFFce0f3d),
+    Color(0xFFfd5e53),
+    Color(0xFFf0134d),
+    Color(0xFFf0134d),
+    
+  ];
   for (int i = 0; i < fetchedStateData.length; i++) {
     stateCardList.add(
       Padding(
@@ -20,7 +66,7 @@ stateCardGenerator() {
             fetchedStateData[i]['state'],
           )),
           decoration: BoxDecoration(
-            color: Color(0xFF000000),
+            color: colors[i],
             borderRadius: BorderRadius.circular(15),
           ),
         ),
@@ -33,7 +79,7 @@ stateCardGenerator() {
           height: 150,
           width: screenWidth,
           decoration: BoxDecoration(
-            color: Color(0xFF000000),
+            color: colors[i],
             borderRadius: BorderRadius.circular(15),
           ),
           child: Column(
